b1d37d4e119c5a80ff70beaec3144b263a543b62
bobmwangih <bobbymwangih@gmail.com>
2022-04-03 07:23:52 +0300

    Updates

---
+++ precache-manifest.25d4cf43f8df6dcf9b5ec56d5ad039eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e959e41b81b141eccb23cc1e32954a43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f9db73eb5b11d8794de",
+    "url": "/static/css/main.af6d9b4f.chunk.css"
+  },
+  {
+    "revision": "3fa965ba68b98f4521c8",
+    "url": "/static/js/2.de589581.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.de589581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9db73eb5b11d8794de",
+    "url": "/static/js/main.1b531a37.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "1eeebd47139c4e99eb673f55cdcc4b7a",
+    "url": "/static/media/about2.1eeebd47.jpg"
+  }
+]);
